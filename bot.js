787b9c57b451899f520bc1c33de83553a4cf62dd
kingta001 <53026135+kingta001@users.noreply.github.com>
2019-07-25 07:55:30 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,38 +1,43 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const myid = ['596778797551058964'];
-const prefix = ['-'];
-console.log("Scrpit By DesTroyeR");
-
-client.on('message', message => {
-    if(message.content === prefix+'راتب'){
-        message.channel.send('#daily')
-    }
+ 
+ 
+ 
+ 
+ 
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setGame(`Queen , | ♦ Free Nitro •`,'https://www.twitch.tv/zya2d_')
+    client.user.setStatus('ldle');
 });
 
-client.on('message', message => {
-    if(message.content === prefix+'مبلغ'){
-        message.channel.send('#credits')
-    }
-});
 
-client.on('message', message => {
-    if(message.content === prefix+'ارسال'){
-        message.channel.send("#credits"+"<@"+ mymid +">"+"2000")
-    }
-});
+	
 
-client.on('message', message => {
-    if(message.content === prefix+'ريب'){
-        message.channel.send("#rep "+"<@" + myid + ">")
-    }
-});
 
-client.on("ready", () => {
-let channel =     client.channels.get("603880113293099010")
-setInterval(function() {
-channel.send(` Hey Pls Give me Rank Iam Helper Man ? `);
-}, 30)
-})
+const adminprefix = "-";
+const devs = ['596778797551058964','600839628739706910'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى ${argresult}**`)
+}
+});
 
 client.login(process.env.BOT_TOKEN);
